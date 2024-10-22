31852b013a61cc9a7ad43319079318f5ad23d9e4
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-10-22 14:32:01 +0200

    Accélération de la compilation !

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
-    content: ["./src/**/!(node_modules)/**/*.{html,js,njk}",
-              "./config/markdown/shortcodes/quotes/!(index).js"],
+    content: [
+        "./src/assets/stylesheets/*.{html,js,njk}",
+        "./src/*.{html,js,njk}",
+        "./src/_includes/**/*.{html,js,njk}",
+        "./src/cs/**/*.{html,js,njk}",
+        "./src/mon/**/*.{html,js,njk}",
+        "./src/pok/**/*.{html,js,njk}",
+        "./src/projets/**/*.{html,js,njk}",
+        "./config/markdown/shortcodes/quotes/!(index).js"
+    ],
     theme: {
       extend: {},
     },
