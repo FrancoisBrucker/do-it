d8a3a198c9a8668ff8517b00d1d9b086fc02f5e4
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-10-24 15:20:04 +0200

    Remaniement du code de configuration

--- .eleventy.js
+++ .eleventy.js
@@ -3,31 +3,20 @@ import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import setupMarkdown from './config/markdown/index.js';
 import setupShortcodes from "./config/markdown/shortcodes/index.js"
 import assetsConfig from "./config/assets.js"
-import searchConfig from "./config/search.js"
-import collectionsConfig from "./config/filters.js";
-import { DateTime } from "luxon";
+import filtersConfig from "./config/filters.js";
+import postCompilation from "./config/search.js"
 
 export default function(eleventyConfig) {
 
   eleventyConfig.addPlugin(EleventyRenderPlugin);
   eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-
-  // eleventyConfig.addPlugin(embedYouTube);
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
   setupMarkdown(eleventyConfig);
   setupShortcodes(eleventyConfig);
   assetsConfig(eleventyConfig);
-  collectionsConfig(eleventyConfig);
-  searchConfig(eleventyConfig);
-
-  eleventyConfig.addFilter('getValueFromPath', function(str, separator, value) {
-    return str.split(new RegExp(separator))[value];
-  });
-
-  eleventyConfig.addFilter("dateFormat", (dateObj, format = "dd/MM/yyyy") => {
-    return DateTime.fromJSDate(dateObj).toFormat(format);
-  });
+  filtersConfig(eleventyConfig);
+  postCompilation(eleventyConfig); // tailwind and search
 
   return {
     dir: {
--- config/search.js
+++ config/search.js
@@ -1,7 +1,13 @@
 import { execSync } from "child_process";
 
 export default function (eleventyConfig) {
+
     eleventyConfig.on('eleventy.after', () => {
-        execSync(`npx pagefind --source dist --glob \"**/*.html\"`, { encoding: 'utf-8' })
+        execSync(`tailwindcss -i ./src/assets/stylesheets/main.css -o ./dist/assets/stylesheets/main.css`)
+    })
+
+    // Search config with pagefind
+    eleventyConfig.on('eleventy.after', () => {
+        execSync(`npx pagefind --site dist --glob \"**/*.html\"`, { encoding: 'utf-8' })
     })
 }
