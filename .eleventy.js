a0db01f9e37077107e54afe456a22c2ee7c8d4ba
Arthur L <18040415+alouradou@users.noreply.github.com>
2024-10-24 15:25:46 +0200

    Renommage d'un fichier de configuration

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,10 @@
-import { EleventyRenderPlugin, EleventyHtmlBasePlugin } from "@11ty/eleventy"
+import { EleventyRenderPlugin, EleventyHtmlBasePlugin } from "@11ty/eleventy";
 import eleventyNavigationPlugin from "@11ty/eleventy-navigation";
 import setupMarkdown from './config/markdown/index.js';
-import setupShortcodes from "./config/markdown/shortcodes/index.js"
-import assetsConfig from "./config/assets.js"
+import setupShortcodes from "./config/markdown/shortcodes/index.js";
+import assetsConfig from "./config/assets.js";
 import filtersConfig from "./config/filters.js";
-import postCompilation from "./config/search.js"
+import postCompilation from "./config/post-build.js";
 
 export default function(eleventyConfig) {
 
